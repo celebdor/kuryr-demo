d913dbf6cce1ecf7f9dc854cc3992e9b7dc1c324
Antoni Segura Puimedon <antonisp@celebdor.com>
2017-08-16 11:30:23 +0200

    make IPv6 and python3 compatible
    Signed-off-by: Antoni Segura Puimedon <antonisp@celebdor.com>

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
-import BaseHTTPServer as http
+try:
+    from http.server import BaseHTTPRequestHandler
+    from http.server import HTTPServer
+except ImportError:
+    from BaseHTTPServer import BaseHTTPRequestHandler
+    from BaseHTTPServer import HTTPServer
+
 import platform
+import socket
 
 
-class Handler(http.BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-Type', 'text/plain')
-    self.end_headers()
-    self.wfile.write("%s: HELLO, I AM ALIVE!!!\n" % platform.node())
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        response = '%s: HELLO, I AM ALIVE!!!\n' % platform.node()
+        self.wfile.write(response.encode('utf-8'))
+
+
+class HTTPServerv6(HTTPServer):
+    address_family = socket.AF_INET6
+
 
 if __name__ == '__main__':
-  httpd = http.HTTPServer(('',8080),Handler)
-  httpd.serve_forever()
+    httpd = HTTPServerv6(('::', 8080), Handler)
+    httpd.serve_forever()
